f8557a8c1d8a39cd22133f171a350cf090a5552e
sergeytodoriko <s.todorikos@gmail.com>
2021-07-27 00:19:49 +0300

    added .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int transfer_sum = 500_000_000;
+        int total = current_balance + transfer_sum;
+        System.out.println(total);
+        System.out.println(total);
+    }
+}
