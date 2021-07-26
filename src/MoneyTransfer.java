5217db3b4b89fb16c4cd3d712e0a93e2213e1a12
sergeytodoriko <s.todorikos@gmail.com>
2021-07-27 01:06:01 +0300

    added Precision.java

--- src/MoneyTransfer.java
+++ src/MoneyTransfer.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MoneyTransfer {
     public static void main(String[] args) {
         int current_balance = 2_000_000_000;
         int transfer_sum = 500_000_000;
